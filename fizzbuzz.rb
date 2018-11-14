38fbdd056cb8f67138061e88ab106a7eac8c9952
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-14 14:54:35 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num) 
+  phrase = ""
+  
+  if num % 3 == 0
+    phrase += "Fizz"
+  end
+  
+  if num % 5 == 0 
+    phrase += "Buzz"
+  end
+    
+  if num % 3 != 0 && num % 5 != 0
+    phrase = nil
+  end
+  
+  return phrase
+end
